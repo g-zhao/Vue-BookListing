7ee6c20e9e217fb11432656504fc420f43ca2147
g-zhao <zhao.gordon@gmail.com>
2021-01-18 23:47:10 -0600

    commit looping through a list of books

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
       <h1>{{title}}</h1>
+      <ul>
+          <li v-for="book in books" v-bind:key="book">{{book.title}} : {{book.author}}</li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
-name: BookList,
-
+name: 'BookList',
 data() {
         return {
-        title: 'All Books'
+        title: 'All Books',
+        books: [
+            {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+            {title: 'American Gods', author: 'Neil Gaiman'},
+            {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+        ]
         }
     }
 }
@@ -22,5 +29,14 @@ data() {
 h1, h2 {
     font-weight: normal;
 }
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: block;
+    margin: 0 10px;
+}
+
 
 </style>
